aceefa3a436333f4a84085434652019f058b0f96
Joshua Hume <valencelectron@hotmail.com>
2021-04-08 10:49:50 +0900

    fixed safari style bugs

--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -2,11 +2,30 @@ import Project from './Project';
 import './Projects.css';
 import toiletImage1 from '../../Assets/toilet-one.png';
 import toiletImage2 from '../../Assets/toilet_two.png';
+import pixelAnimator from '../../Assets/pixel-animator-2.png';
+import pixelAnimation from '../../Assets/pixel-animator.gif';
+import ohb from '../../Assets/ohb-screenshot.png';
+import ohbTwo from '../../Assets/drum-machine.png';
 
 const Projects: React.FC = () => {
   return (
     <div className="projects-container">
-      <h1>Projects</h1>
+      <h2>Projects</h2>
+      <Project
+        title={'Pixel Art Animator'}
+        description={
+          <p>
+            {' '}
+            As a programmer of retro-styled 2d games, I always wanted a simple and easy to use pixel-art sprite sheet
+            generator. Since I never found one that that suited my needs, I decided to make it myself! Pixel Art
+            Animator allows users to easily make animations and save them as either animated gifs or png sprite sheets.
+          </p>
+        }
+        imageSources={[pixelAnimator, pixelAnimation]}
+        sourceCode={'https://github.com/HatAndBread/pixel-animator'}
+        site="https://www.pixel-art-animator.com/"
+        tech="Create-React-App"
+      />
       <Project
         title={'Tokyo Toilet Accessibility Map'}
         description={
@@ -20,7 +39,22 @@ const Projects: React.FC = () => {
         imageSources={[toiletImage1, toiletImage2]}
         sourceCode={'https://github.com/HatAndBread/tokyo-toilet-accessibility-map'}
         site="https://tokyo-toilet-map.herokuapp.com/"
-        tech="React ⭐️ Ruby On Rails ⭐️ MapBox"
+        tech="React ⭐️ Ruby On Rails ⭐️ MapBox GL"
+      />
+      <Project
+        title={'One Hand Band'}
+        description={
+          <p>
+            {' '}
+            One Hand Band is a progressive web app that allows users to play music wth friends across borders and
+            overseas in real-time with the aid of web sockets. Imagine a chat-room (remember those?) in which your only
+            mode of communication is music.
+          </p>
+        }
+        imageSources={[ohb, ohbTwo]}
+        sourceCode={'https://github.com/HatAndBread/One-Hand-Band'}
+        site="https://one-hand-band.glitch.me/"
+        tech="React ⭐️ Express ⭐️ SQLite3 ⭐️ Socket IO ⭐️ Tone JS "
       />
     </div>
   );
--- src/Components/SideMenu/SideMenu.tsx
+++ src/Components/SideMenu/SideMenu.tsx
@@ -7,39 +7,43 @@ import gitHub from '../../Assets/github.png';
 
 const SideMenu: React.FC = () => {
   return (
-    <div className="side-menu-container">
-      <img id="me" src={Me} alt="A very handsome gentleman" />
-      <h1>Joshua Hume</h1>
-      <p className="side-menu-text">
-        Hello. My name is Joshua and I am a Tokyo-based <span style={{ textDecoration: 'line-through' }}>nerd</span>{' '}
-        computer programmer. I like making things, breaking things, and putting them back together again. Please have a
-        look through my projects.
-      </p>
-      <h3>Programming</h3>
-      <div className="side-menu-text side-menu-list">
-        JavaScript ⭐️ TypeScript ⭐️ Ruby ⭐️ Rails ⭐️ React ⭐️ SQL ⭐️ Web Audio ⭐️ Phaser
-      </div>
-      <h3>Cycling‍</h3>
-      <div className="side-menu-text">
-        Unbeknownst to many I am an adept bicycle mechanic, restorer of vintage bicycles, explorer of back roads, and
-        maker of maps.
-      </div>
-      <h3>Music</h3>
-      <div className="side-menu-text">
-        I am the founder and sole full-time member of the Tokyo / Austin based experimental music group{' '}
-        <a href="https://soundcloud.com/lead-sister-ii">Lead Sister II.</a>
-      </div>
-      <h3>Contact</h3>
-      <div className="side-menu-text sns">
-        <a href="https://www.linkedin.com/in/joshua-hume-0259691ab/">
-          <img src={linkedIn} alt="linked" />
-        </a>
-        <a href="https://github.com/HatAndBread">
-          <img src={gitHub} alt="github" />
-        </a>
-        <a href="mailto: valencelectron@hotmail.com">
-          <img src={email} alt="email" />
-        </a>
+    <div className="sticky-container">
+      <div className="side-menu-container">
+        <img id="me" src={Me} alt="A very handsome gentleman" />
+        <h1>Joshua Hume</h1>
+        <p className="side-menu-text">
+          Hello. My name is Joshua and I am a Tokyo-based <span style={{ textDecoration: 'line-through' }}>nerd</span>{' '}
+          computer programmer. I like making things, breaking things, and putting them back together again. Please have
+          a look through my projects.
+        </p>
+        <h3>Programming</h3>
+        <div className="side-menu-text side-menu-list">
+          JavaScript ⭐️ TypeScript ⭐️ Ruby ⭐️ Rails ⭐️ React ⭐️ SQL ⭐️ Web Audio ⭐️ Phaser
+        </div>
+        <h3>Cycling‍</h3>
+        <div className="side-menu-text">
+          Unbeknownst to many I am an adept bicycle mechanic, restorer of vintage bicycles, explorer of back roads, and
+          maker of maps.
+        </div>
+        <h3>Music</h3>
+        <div className="side-menu-text">
+          I am the founder and sole full-time member of the Tokyo / Austin based experimental music group{' '}
+          <a href="https://soundcloud.com/lead-sister-ii">Lead Sister II.</a>
+        </div>
+        <h3>Contact</h3>
+        <div className="sns-box">
+          <div className="side-menu-text" style={{ marginBottom: '60px' }}>
+            <a href="https://www.linkedin.com/in/joshua-hume-0259691ab/">
+              <img src={linkedIn} alt="linked" />
+            </a>
+            <a href="https://github.com/HatAndBread">
+              <img src={gitHub} alt="github" />
+            </a>
+            <a href="mailto: valencelectron@hotmail.com">
+              <img src={email} alt="email" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
